019ef5da33a804c47a6879a510b4447119cee31e
Nikita Korovkin <twodrops@mail.ru>
2024-03-21 17:02:21 +0700

    new def: get_card

--- backend/htmlproj/polls/views.py
+++ backend/htmlproj/polls/views.py
@@ -8,17 +8,27 @@ def index(request):
     user = request.user
     list_of_templates = Templates.objects.filter(user=user)
 
-    return redirect('', {'user': user, 'list_of_templates':list_of_templates})
+    return redirect('', {'user': user, 'list_of_templates': list_of_templates})
 
 
 def delete_template(request):
     template_id = request.GET.get('template_id')
-    status=200
+    status = 200
     print('input', str(template_id))
     try:
         Templates.objects.filter(id=template_id).delete()
         print('deleted')
     except:
         print('fail')
-        status=400
+        status = 400
     return JsonResponse({'status': status, })
+
+def get_card(request):
+    template_id = request.GET.get('template_id')
+    exist = Templates.objects.filter(id=template_id)[0]
+    if exist:
+        HTML_text = exist.HTML_page
+    else:
+        HTML_text = ''
+    return JsonResponse({'text': HTML_text, })
+
