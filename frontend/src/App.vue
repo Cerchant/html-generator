80793558593b5d92c66ebe46f8dee57b10191121
julia_yarlykova <115877136+JuliaYarlykova@users.noreply.github.com>
2024-03-21 11:13:19 +0700

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import mainBoard from './components/mainBoard.vue';
+import page_header from './components/page_header.vue';
+import topPanel from './components/topPanel.vue';
+
+</script>
+
+<template>
+  <page_header ></page_header>
+  <main>
+    <topPanel></topPanel>
+    <mainBoard></mainBoard>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ frontend/src/components/topPanel.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="top-panel">
+        <div class="top-panel__container">
+            <a href="#" class="link">
+                <div class="link_img">
+                    <img src="../assets/Group.svg" alt="" class="img">
+                </div>
+                
+            </a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.top-panel__container{
+    display: flex;
+    flex-direction: row;
+    position: relative;
+    align-items: center;
+    width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+    background-color: #BABDBF;
+    height: 50px;
+    overflow-x: hidden;
+}
+.link_img{
+    margin-left: 49px;
+    width: 35px;
+    height: 35px;
+}
+.img{
+    width: 100%;
+    height: 100%;
+}
+</style>
