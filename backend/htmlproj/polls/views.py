328bef4b8a9ff5a0617ec83b148d5d9935e2df28
Nikita Korovkin <twodrops@mail.ru>
2024-03-21 14:52:46 +0700

    new def: delete_template new def: index

--- backend/htmlproj/polls/views.py
+++ backend/htmlproj/polls/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.http import HttpResponse, JsonResponse
+
+
+def index(request):
+    user = request.user
+    list_of_templates = Templates.objects.filter(user=user)
+
+    return redirect('', {'user': user, 'list_of_templates':list_of_templates})
+
+
+def delete_template(request):
+    template_id = request.GET.get('template_id')
+    status=200
+    print('input', str(template_id))
+    try:
+        Templates.objects.filter(id=template_id).delete()
+        print('deleted')
+    except:
+        print('fail')
+        status=400
+    return JsonResponse({'status': status, })
